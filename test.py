d54cbf8eefb8f21831c8e5ab1eebaac9308592bd
SLilani <lilani.sofiane@hotmail.fr>
2017-03-16 10:35:15 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+import pandas as pd
+from datetime import datetime as dt
+import matplotlib.pyplot as plt
+#get_ipython().magic(u'matplotlib inline')
+import math
+import random
+import outils
+
+
+# In[2]:
+
+outils.testVerticalProc()
+
+
+# In[3]:
+
+outils.testVerticalCenterProc()
+
+
+# In[4]:
+
+outils.testHorizontalProc()
+
+
+# In[5]:
+
+outils.testHorizontalCenterProc()
+
+
+# In[6]:
+
+outils.testCrosseProc()
+
+
+# In[7]:
+
+outils.simpleTestProc()
+
